f323f68fb0fab64b03018c4a58363859ea514dd8
ajay2vj <vjajay98@gmail.com>
2022-09-10 23:22:28 +0530

    navbar and below component added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import Navbar from './components/navbar';
-// import './App.css';
+import classes from './App.module.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-6'>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </div>
-          <div className='col-md-6'>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <div className={classes.parentPadding}>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-10'>
+              <div>
+                <h3>Analytics Dashboard</h3>
+                <span>Dashboard is an online report where your website data from ecommerce to web anayltics is displayed.</span>
+              </div>
+            </div>
+            <div className='col-md-2'>
+              <div className='flex gap-3 justify-center'>
+                <Button 
+                  type='primary'
+                >Star</Button>
+                <Button 
+                  type='primary'
+                >Create New</Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -3,14 +3,17 @@ import logo from '../../logo.svg'
 import classes from './style.module.css'
 export default function Navbar(){
   return(
-    <nav className="navbar bg-light fixed-top">
+    <nav className={`navbar bg-light fixed-top ${classes.nav_shadow}`}>
       <div className="container-fluid">
         <div className="navbar-brand">
-          <img src={logo} alt='logoImage' className={classes.image_width}/>
+          <div className='flex'>
+            <img src={logo} alt='logoImage' className={classes.image_width}/>
+            <h1 className={classes.text_react}>React</h1>
+          </div>
         </div>
-        <div className='flex'>
-          <p className='underline'>John Wick</p>
-          <span>Developer</span>
+        <div className='inline-grid mr-3'>
+          <span className='text-base font-medium'>John Wick</span>
+          <span className='text-xs text-center'>Developer</span>
         </div>
       </div>
     </nav>
