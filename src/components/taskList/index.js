0aa6e9c739c287adfe35e1d161620a4af48688ed
ajay2vj <vjajay98@gmail.com>
2022-09-11 19:41:35 +0530

    toast message added

--- src/components/taskList/index.js
+++ src/components/taskList/index.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from '../../App.module.css'
 import { FiDatabase } from 'react-icons/fi'
 import Task from "../task";
 import { taskData } from "../../mock";
-import { Button } from "antd";
+import { Button, Popconfirm } from "antd";
 import { Draggable } from "react-drag-reorder";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function TaskList(){
+  const [dragg, setDragg] = useState(false)
   const getOnchange = (curr, newProp)=>{
-    // console.log(curr, 'curr')
-    // console.log(newProp, 'newProp')
+    // setDragg(newProp ? true: false)
+  }
+  const onCancel = ()=>{
+    setDragg(false)
+  }
+  const handleOk =()=>{
+    toast.success('You have clicked save!', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    setDragg(false)
   }
   return(
     <div className={`col-md-6`}>
+      <ToastContainer />
       <div className={`mb-3 ${classes.column_chat}`}>
         <div className='p-3 flex gap-2'>
           <FiDatabase
@@ -23,7 +41,10 @@ export default function TaskList(){
           </span>
         </div>
         <hr className='mb-1'/>
-        <Draggable onPosChange={getOnchange}>
+        <Draggable onPosChange={(e)=>{
+          setDragg(e === 0 || e ? true: false)
+          getOnchange(e)}}
+        >
           {taskData?.map((item, index)=>(
             <div key={index}>
               <Task 
@@ -37,10 +58,20 @@ export default function TaskList(){
             </div>
           ))}
         </Draggable>
-        <div className="flex justify-end gap-2 p-3">
-          <Button>Cancel</Button>
-          <Button type="primary">Save</Button>
-        </div>
+        {dragg ? 
+          <div className="flex justify-end gap-2 p-3">
+            <Button onClick={onCancel}>Cancel</Button>
+            <Popconfirm 
+              title="Are you sure？" 
+              okText="Yes" 
+              cancelText="No" 
+              onConfirm={handleOk}
+            >
+              <Button type="primary">Save</Button>
+            </Popconfirm>
+          </div>
+        : null}
+        
       </div>
     </div>
   )
