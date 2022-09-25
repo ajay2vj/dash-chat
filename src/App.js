348dd0a6c324a0f92f6a460d55f79e656758a50f
ajay2vj <vjajay98@gmail.com>
2022-09-25 13:09:51 +0530

    react route added

--- src/App.js
+++ src/App.js
@@ -1,69 +1,14 @@
-import Navbar from './components/navbar';
-import classes from './App.module.css';
-import { Button } from 'antd';
-import { TbPlaneDeparture } from 'react-icons/tb'
-import { AiOutlineStar } from 'react-icons/ai'
-import { HiPlusSm } from 'react-icons/hi'
-import TaskList from './components/taskList';
-import ChatBox from './components/chatbox';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import Ecommerce from './pages/ecommerce';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <div className={classes.parentPadding}>
-        <div className={classes.background_Color}>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-10'>
-                <div className='flex'>
-                  <div className={classes.icon}>
-                    <TbPlaneDeparture 
-                      size={25}
-                      color="#61dafb"
-                    />
-                  </div>
-                  <div className={`pb-3 inline-grid ${classes.mobile_res}`}>
-                    <span className='text-lg font-medium'>Analytics Dashboard</span>
-                    <span>Dashboard is an online report where your website data from ecommerce to web anayltics is displayed.</span>
-                  </div>
-                </div>
-              </div>
-              <div className='col-md-2'>
-                <div className='flex gap-3 justify-center'>
-                  <Button 
-                    type='primary'
-                    style={{background: '#000', border: '#000'}}
-                  >
-                    <AiOutlineStar 
-                      size={22}
-                    />
-                  </Button>
-                  <Button 
-                    type='primary'
-                    style={{display: 'flex', background: '#3fb056', border: '#3fb056'}}
-                  >
-                    <HiPlusSm 
-                      size={18}
-                      fill="#fff"
-                      className='mt-0.5 mr-1'
-                    /> Create New
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={classes.task_chat}>
-          <div className='container'>
-            <div className='row'>
-              <TaskList />
-              <ChatBox />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path='/' element={<Ecommerce />}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/ecommerce.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Ecommerce(){
+  return(
+    <div>
+      Ecommerce
+    </div>
+
+  )
+}
