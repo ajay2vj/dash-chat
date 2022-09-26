a8a5d89746e3dfac9d30a1a1ccf0c2cf9289a811
ajay2vj <vjajay98@gmail.com>
2022-09-26 22:22:33 +0530

    quantity count

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -2,15 +2,58 @@ import React, { useState } from 'react'
 import Accordion from '../accordion';
 import CartEmpty from '../cart_empty';
 import './product.css';
+import { AiOutlineShoppingCart, AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai'
+
 export default function Product({content}){
   const [passData, setPassData] = useState([])
-  // console.log(passData)
+  const [count, setCount] = useState(0)
+  
+  // const transFormData = passData?.map((item, index)=>({
+  //   id: item?.id,
+  //   fnbid: item?.fnbid,
+  //   show: item?.show,
+  //   cinemaid: item?.cinemaid,
+  //   priceincents: item?.priceincents,
+  //   strikeprice: item?.strikeprice,
+  //   strikeValueBeforeTax: item?.strikeValueBeforeTax,
+  //   strikeTaxValue: item?.strikeTaxValue,
+  //   amount: item?.amount,
+  //   maximumqty: item?.maximumqty,
+  //   product_qty: 0,
+  //   promoonly: item?.promoonly,
+  //   active: item?.active,
+  //   order: item?.order,
+  //   tax: item?.tax,
+  //   taxValue: item?.taxValue,
+  //   valuebeforetax: item?.valuebeforetax,
+  //   pickupAtCounter: item?.pickupAtCounter,
+  //   inSeatDelivery: item?.inSeatDelivery,
+  //   fnbs_images: item?.fnbs_images,
+  //   itemName: item?.itemName,
+  //   itemDetails: item?.itemDetails,
+  //   currency: item?.currency,
+  //   modifierGroups: item?.modifierGroups,
+  //   smartModifiers: item?.smartModifiers,
+  //   fnbs_alternateitems: item?.fnbs_alternateitems
+  // }))
+
+  // const Increment = (item, index)=>{
+  //   console.log(item)
+  //   setCount(transFormData?.map((itm)=>
+  //     item?.id === itm?.id ? {...item, product_qt: item?.product_qty + 1} : item
+  //   ))
+  // }
+  // const Decrement = (item, index)=>{
+  //   setCount(transFormData?.map((itm)=>
+  //     item?.id === itm?.id ? {...item, product_qt: item?.product_qty - 1} : item
+  //   ))
+  // }
   return(
     <div className='px-5 product-grid'>
       <div>
         {content?.map((item, index)=>
           <div className="card" key={index}>
-            <img src={item?.fnbtabs_images?.map((item)=> item?.imageurl)} alt="5 Terre" width={'100%'} />
+            <img src={item?.fnbtabs_images?.map((item)=> item?.imageurl)} alt="product thumbnail" width={'100%'} />
             <div className="accordion">
             <Accordion 
               title={item?.tabName}
@@ -24,13 +67,31 @@ export default function Product({content}){
       <div className='order-summary'>
         {passData?.length > 0 ? (
           <div className='p-4'>
-            <p className='flex justify-start text-lg'>Order Summary</p>
-            <p className='flex justify-start text-base underline'>Picked-up by you</p>
+            <p className='flex justify-start text-lg font-medium select-none'>Order Summary</p>
+            <p className='select-none flex justify-start text-base underline decoration-red-500 font-medium'>
+            <AiOutlineShoppingCart 
+              size={25}
+              color="#ff4d4f"
+            /> &nbsp;&nbsp;Picked-up by you
+            </p>
             {passData?.map((item, index)=>
               <div key={index}>
-                <div className='flex justify-between'>
-                  <span>{item?.itemName}</span>
-                  <span>{item?.valuebeforetax}</span>
+                <div className='grid-cart'>
+                  <span className='font-medium select-none'>{item?.itemName}</span>
+                  <div className='flex gap-2'>
+                    <AiOutlineMinusCircle 
+                      size={25}
+                      color="#ff4d4f"
+                      onClick={(e)=> {e.stopPropagation(); setCount(count - 1)}}
+                    />
+                    <span className='select-none'>{count}</span>
+                    <AiOutlinePlusCircle 
+                      size={25}
+                      color="#ff4d4f"
+                      onClick={(e)=> {e.stopPropagation(); setCount(count + 1)}}
+                    />
+                  </div>
+                  <span className='font-medium select-none'>${item?.valuebeforetax}</span>
                 </div>
               </div>
             )}
